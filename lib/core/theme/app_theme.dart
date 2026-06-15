b34f8eb373fe0bc4702b1dec97185725274edd05
Abu Backer Siddique M <siddique2k6@gmail.com>
2026-06-16 00:03:20 +0530

    Fix CardThemeData to CardTheme in app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -48,7 +48,7 @@ class AppTheme {
         ),
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      cardTheme: CardThemeData(
+      cardTheme: CardTheme(
         elevation: 4,
         shadowColor: Colors.black.withOpacity(0.1),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
